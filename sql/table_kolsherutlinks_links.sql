d310b42c1e10df74eb40e073faafe6ac609789ba
drorsnir <FreedomFighterSparrow@gmail.com>
2024-05-06 15:49:14 +0300

    Fix DB column for link_id - should be unsigned
    Otherwise, it doesn't match the foreign keys, and the install fails.

--- sql/table_kolsherutlinks_links.sql
+++ sql/table_kolsherutlinks_links.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS /*_*/kolsherutlinks_links (
-	link_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	link_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	url BLOB NOT NULL,
 	text TEXT NOT NULL
 ) /*$wgDBTableOptions*/;
